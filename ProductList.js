5600f3d058fa553caa2f9c0ef2fdee6ea8fcd670
Blake <blakewalters53@gmail.com>
2024-06-13 22:57:39 -0400

    Create ProductList.js

---
+++ ProductList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProductList = ({ addToCart }) => {
+  const products = [
+    { id: 1, name: 'Product A', price: 10 },
+    { id: 2, name: 'Product B', price: 20 },
+    { id: 3, name: 'Product C', price: 30 }
+  ];
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
